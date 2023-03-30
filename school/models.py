7b45fe8354e32d0721c578e280a46a8763f0c2e0
SerjeiMikailov <felipemaders8@gmail.com>
2023-03-30 20:45:41 -0300

    g

--- school/models.py
+++ school/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=35)
+    rg = models.CharField(max_length=9)
+    cpf = models.CharField(max_length=35)
+    birth = models.DateField()
+
+    def __str__(self):
+        return self.name
+    
+
+class Course(models.Model):
+    Level = (
+        ('B', 'Basic'),
+        ('I', 'Intermediate'),
+        ('A', 'Advanced')
+    )
+    code_course = models.CharField(max_length=10)
+    description = models.CharField(max_length=200)
+    level_course = models.CharField(max_length=10)
